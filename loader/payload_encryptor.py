c7b191b690b18596d7f151f2485f41a499b677d3
smmiillee <smilejpgcsgo@gmail.com>
2026-09-12 03:39:12 -0400

    Fix return statement formatting in payload_encryptor.py

--- loader/payload_encryptor.py
+++ loader/payload_encryptor.py
@@ -13,7 +13,6 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-
 class PayloadEncryptor:
     def __init__(self, encryption_secret: bytes):
         self._encryption_secret = encryption_secret
